fbe4653ff6c2c92c090c3dc3a665a17058b0c9c7
david1348 <sachdavid15@gmail.com>
2026-04-27 09:44:25 +0200

    Update ap.py

--- ap.py
+++ ap.py
@@ -1,41 +1,146 @@
-import psycopg2
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# test route
+@app.route('/')
+def home():
+    return "Vitajte!"
 
 databaza = {
     "students": [
-        {"id": 1, "name": "Adrián", "surname": "Červenka", "nickname": "ChillyHotPpr"},
-        {"id": 2, "name": "Karolína", "surname": "Kmeťová", "nickname": "null"},
-        {"id": 3, "name": "Matej", "surname": "R", "nickname": "T-34"},
-        {"id": 4, "name": "Milan", "surname": "K", "nickname": "Lopta"},
-        {"id": 5, "name": "Dávid", "surname": "Š", "nickname": "DVD"},
-        # ... rest of your data
+        {  
+            "id": 1,
+            "name": "Adrián",
+            "surname": "Červenka",
+            "nickame": "ChillyHotPpr"
+        },
+        {
+            "id": 2,
+            "name": "Karolína",
+            "surname": "Kmeťová",
+            "nickame": "null"
+        },
+        {
+            "id": 3,
+            "name": "Matej",
+            "surname": "R",
+            "nickame": "T-34"
+        },
+        {  
+            "id": 4,
+            "name": "Milan",
+            "surname": "K",
+            "nickame": "Lopta"
+        },
+        {
+            "id": 5,
+            "name": "Dávid",
+            "surname": "Š",
+            "nickame": "DVD"
+        },
+        {
+            "id": 6,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {  
+            "id": 7,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {
+            "id": 8,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {
+            "id": 9,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {  
+            "id": 10,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {
+            "id": 11,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {
+            "id": 12,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {  
+            "id": 13,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {  
+            "id": 14,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {
+            "id": 15,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {
+            "id": 16,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {  
+            "id": 17,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {  
+            "id": 18,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {
+            "id": 19,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        {
+            "id": 20,
+            "name": "",
+            "surname": "",
+            "nickame": ""
+        },
+        
     ]
 }
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="your_db",
-    user="your_user",
-    password="your_password",
-    port=5432
-)
+@app.route('/api')
+def api():
+    return jsonify(databaza)
 
-cur = conn.cursor()
+@app.route('/api/student/<int:student_id>')
+def find_student(student_id):
+    student = databaza["students"][student_id - 1]
+    return jsonify(student)
 
-for student in databaza["students"]:
-    cur.execute(
-        """
-        INSERT INTO students (id, name, surname, nickname)
-        VALUES (%s, %s, %s, %s)
-        ON CONFLICT (id) DO NOTHING;
-        """,
-        (
-            student["id"],
-            student["name"],
-            student["surname"],
-            student["nicnkame"] 
-        )
-    )
-
-conn.commit()
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    app.run(debug=True)
